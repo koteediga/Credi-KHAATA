09337a9727a4d7df76f5e33fcb7f3efadb060f81
Ediga Koteswararao <105491279+koteediga@users.noreply.github.com>
2025-05-04 13:23:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Login from './components/Login';
+ import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Dashboard from './components/Dashboard';
 import CustomerDetails from './components/CustomerDetails';
 import AddLoanForm from './components/AddLoanForm';
+import AddCustomerForm from './components/AddCustomerForm';
+//import { ToastContainer } from './components/ToastContainer';
 import Navbar from './components/Navbar';
 import { AuthProvider } from './Context/AuthContext';
 
 
 function App() {
   return (
-    
+    // <>
+    //    <Navbar/>
+    //   <h2>kote</h2>
+    // </>
     <AuthProvider>
       <Router>
         <Navbar />
+               
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/customer/:id" element={<CustomerDetails />} />
           <Route path="/add-loan" element={<AddLoanForm />} />
+          <Route path="/add-customers" element={<AddCustomerForm />} />
+           <Route path="/customerdetails" element={<CustomerDetails/>}/>
         </Routes>
       </Router>
     </AuthProvider>
