d9c283f1ff229f57b4a2a67a5b465215eebcef86
Ediga Koteswararao <105491279+koteediga@users.noreply.github.com>
2025-05-04 13:21:24 +0530

    Update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Navbar() {
+const Navbar=()=>{
   return (
     <nav className="bg-blue-600 p-4">
       <ul className="flex space-x-4 text-white">
         <li><Link to="/dashboard">Dashboard</Link></li>
         <li><Link to="/add-customer">Add Customer</Link></li>
         <li><Link to="/add-loan">Add Loan</Link></li>
-        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/">Login</Link></li>
+        <li><Link to="/signup">SignUp</Link></li>
+        <li><Link to="/customerdetails">customer_details</Link></li>
       </ul>
     </nav>
   );
