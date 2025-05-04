24b0745b57ed49d299c0b5d0f09f1c6fbd922a16
Ediga Koteswararao <105491279+koteediga@users.noreply.github.com>
2025-05-04 13:22:59 +0530

    Update Toast

---
+++ src/components/Toast.js
@@ -0,0 +1,8 @@
+import { toast } from 'react-toastify';
+
+// Example function to trigger a toast notification
+const ToastContainer = () => {
+  toast.success("Operation successful!");
+};
+
+export default ToastContainer;
